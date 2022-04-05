9ce6c35e2034d2572cb1d7e1108bc9cb022190cd
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-04-05 14:39:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Calc
+{
+    public int Add(int i, int j)
+    {
+        return i + j;
+    }
+    public int Add(int i, int j, int k)
+    {
+
+        return i + j + k;
+
+    }
+
+
+    public String Add(string f1, String f2)
+    {
+        return f1 + f2;
+    }
+
+    static void Main()
+    {
+        Calc ob = new Calc();
+        ob.Add(1, 2);
+        ob.Add("Ram", "Lakhan");
+        ob.Add(10, 34, 23);
+        Console.WriteLine(ob.ToString());
+        Console.ReadKey();
+    }
+}
